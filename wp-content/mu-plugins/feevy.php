d41908a8f3dec9b1fa1d38eecb648acd58c1c8ae
Alexandre Girard <alx.girard@gmail.com>
2008-07-04 14:54:24 +0200

    add various mu-plugins

---
+++ wp-content/mu-plugins/feevy.php
@@ -0,0 +1,466 @@
+<?php
+/*
+Plugin Name: Feevy
+Plugin URI: http://www.feevy.com
+Description: Feevy plugin for Wordpress Mu
+Version: 1.0
+Author: Alexandre Girard
+Author URI: http://blog.alexgirard.com
+*/
+
+
+
+/*
+* =========
+*  HEADER
+* =========
+*/
+include "feevy_soup.php";
+define('API_KEY','52326fc86dce42ca84ec82a0b7a0f4f04ff3c50b');
+define('FEEVY_CODE','6362');
+
+
+
+/*
+* ===========
+*  Feevy API 
+* ===========
+*/
+
+/**
+* Get blog details on feevy
+* Parameters: 
+* 	- $blog_id: id of the blog to get on feevy
+*/
+function feevy_get_blog($blog_id) {
+	switch_to_blog($blog_id);
+	
+	// Set request to Feevy API
+    $api = new feevySoup;
+    $api->api_key = API_KEY;
+    $api->type = 'get_feed';
+	$api->params = array('feed_url' => "http://feevy.bbvablogs.com/feed/");
+	
+	// Execute request
+	$content = $api->get_content();
+	
+	restore_current_blog();
+	
+	return $content['result']['feed'];
+}
+
+/**
+* Add blog to feevy
+* Parameters: 
+* 	- $blog_id: id of the blog to add to feevy
+*/
+function feevy_add_blog($new_blog_id) {
+	
+	switch_to_blog($blog_id);
+	
+	// Set request to Feevy API
+    $api = new feevySoup;
+    $api->api_key = API_KEY;
+    $api->type = 'add_feed';
+	$api->params = array('href' => get_option('home'), 'url' => get_feed_link('rss2'));
+	
+	// Execute request
+	$api->get_content();
+	
+	restore_current_blog();
+}
+
+
+
+/**
+* Update blog tags on feevy
+* Parameters: 
+* 	- $blog_id: id of the blog to update
+*	- $tags: tags list to set
+*/
+function feevy_update_tag($blog_id, $tags){
+	
+	// Get blog from blog_id
+	$blog_details = get_blog_details($blog_id);
+	
+	if(strlen($blog_details->domain) == 0 or $blog_details->deleted == 1 or $blog_details->spam == 1)
+		return false;
+	
+	$feed_url = $blog_details->siteurl."/feed/";
+	
+	// Set request to Feevy API
+    $api = new feevySoup;
+    $api->api_key = API_KEY;
+    $api->type = 'edit_tags';
+	$api->params = array('feed_url' => $feed_url, 
+						 'tag_list' => $tags);
+	
+	// Execute request
+	$api->get_content();
+}
+
+
+
+/**
+* Update blog tags on feevy when user edit his profile
+* Parameters: 
+* 	- $user_id: id of the blog to update
+*/
+function feevy_update_tag_from_user($blog_id){
+	
+	// Get first user from blog
+	$user = array_pop(get_users_of_blog($blog_id));
+	
+	// Build tag list
+	$tags = user_meta_to_feevy_tags($user->user_id);
+	
+	// update feevy 
+	feevy_update_tag($blog_id, $tags);
+}
+
+
+
+/**
+* Build tags list from user metadata
+* Parameters: 
+* 	- $user_id: user to fech metadata from
+*/
+function user_meta_to_feevy_tags($user_id) {
+	$tags = "";
+	
+	// collect tags from user
+	$pais = get_usermeta( $user_id, "pais" );
+	if(strlen($pais) > 0)
+		$tags .= "+$pais";
+		
+	// You might want to check count($blog_users) to avoid corporative sexuality 
+	$sex = get_usermeta( $user_id, "genero" );
+	if(strcmp($sex,'Masculino') == 0){
+		$tags .= "+Hombre";
+	} 
+	if(strcmp($sex,'Femenino') == 0){
+		$tags .= "+Mujer";
+	}
+	
+	// Area tag
+	$area = get_usermeta($user_id, 'area');
+	if(strlen($area) > 0)
+		$tags .= "+$area";
+	
+	// Unidad tag
+	$unidad = get_usermeta($user_id, 'unidad');
+	if(strlen($unidad) > 0)
+		$tags .= "+$unidad";
+	
+	return preg_replace('/\+(.*)/','$1',$tags);
+}
+
+
+
+/**
+* Update blog avatar on feevy
+* Parameters: 
+* 	- $user_id: user to fech metadata from
+*/
+function feevy_update_avatar(){
+	global $user_ID;
+	
+	$avatar_url = author_image_path($user_ID, false);
+	
+	// Check if user has an avatar
+	if(strlen($avatar_url) > 0){
+		
+		// Get list of user blogs
+		$blogs = get_blogs_of_user($user_ID);
+		
+		// Set avatar for each blog
+		foreach($blogs as $blog){
+			
+			$user_count = count(get_users_of_blog( $blog->userblog_id ));
+			
+			// Verify blog url
+			if($user_count == 1 and strlen($blog->siteurl) > 0) {
+				
+				// Set request to Feevy API
+				$api = new feevySoup;
+			    $api->api_key = API_KEY;
+			    $api->type = 'edit_avatar';
+				$api->params = array('feed_url' => "$blog->siteurl/feed/", 
+									 'avatar_url' => $avatar_url);
+
+				// Execute request
+				$api->get_content();
+			}
+		}
+	}
+}
+
+
+
+/**
+* Ping Feevy when a new post is published
+*/
+function feevy_ping(){
+	global $wpdb;
+	switch_to_blog($wpdb->blogid);
+	$params = array('http' => array('method' => 'POST', 'content' => 'url='.urlencode(get_feed_link('rss2'))));
+    @fopen('http://www.feevy.com/ping/update', 'rb', false, stream_context_create($params));                                                           
+	restore_current_blog();
+}
+
+
+
+/*
+* ==============
+*  Feevy script 
+* ==============
+*/
+
+/**
+* Generate javascript code to display Feevy
+* Parameters: 
+* 	- $display: return value only if false
+*	- $style: style to display feevy with
+*	- $_POST: set area, unidad, pais and sexo to display
+*/
+function feevy_code($display = true, $style = "white"){
+	
+	// Initialize feevy code
+	$feevy = "<script type='text/javascript' src='http://www.feevy.com/code/".FEEVY_CODE;
+	
+	// Set area tag
+	if (strlen($_POST['area']) > 0 ) {
+		$area = $_POST['area'];
+		$feevy .= "/tags/$area";
+	}
+	
+	// Set unidad tag
+	if (strlen($_POST['unidad']) > 0 ){
+		$unidad = $_POST['unidad'];
+		if(stripos($feevy, "tags")) {
+			$feevy .= "+$unidad";
+		}else{
+			$feevy .= "/tags/$unidad";
+		}
+	}
+	
+	// Set pais tag
+	if (strlen($_POST['pais']) > 0 ){
+		$pais = $_POST['pais'];
+		if(stripos($feevy, "tags")) {
+			$feevy .= "+$pais";
+		}else{
+			$feevy .= "/tags/$pais";
+		}
+	}
+
+	// Set sex tag
+	if (strlen($_POST['sexo']) > 0 ){
+		$sexo = $_POST['sexo'];
+		if(stripos($feevy, "tags")) {
+			$feevy .= "+$sexo";
+		}else{
+			$feevy .= "/tags/$sexo";
+		}
+	}
+	
+	// Close code
+	$feevy .= "/$style'></script>";
+	
+	if($display){
+		echo $feevy;
+	} else {
+		return $feevy;
+	}
+}
+
+/*
+* ===============
+*  Feevy options 
+* ===============
+*/
+
+/**
+* Add Feevy option panel
+*/
+function feevy_options() {
+	if (function_exists('add_options_page')) {
+		add_options_page(
+			__('Feevy Portal Options')
+			, __('Feevy Portal')
+			, 10
+			, basename(__FILE__)
+			, 'feevy_options_form'
+		);
+	}
+}
+
+/**
+* Print Feevy option panel form
+*/
+function feevy_options_form() {
+	global $blog_id;
+	$feed = feevy_get_blog($blog_id);
+	print('
+		<div class="wrap">
+			<h2>'.__('Feevy Portal Options').'</h2>
+			<p>You can update your Feevy options for your blog here, they will then appear in <a href="http://feevy.bbvablogs.com">Feevy Portal</a>.</p>
+			<form name="feevy_portal" action="'.get_bloginfo('wpurl').'/wp-admin/options-general.php?page=feevy.php" method="post">
+				'.wp_nonce_field("feevy-update-options").'
+				<input type="hiddent" name="feevy_portal_update" value="feevy_options_update" id="feevy_options_update">
+				<fieldset class="options">
+					<legend>Tag list</legend>
+				</fieldset>
+				'.add_avatar_fields($feed['avatar']).'
+				<p class="submit">
+					<input type="submit" name="submit" value="'.__('Update Your Feevy').'" />
+				</p>
+			</form>
+		</div>
+	');
+}
+
+function add_avatar_fields($current_avatar) {
+	global $user_ID, $image_extensions, $profile_picture_options;
+	// build extension check string for the js
+	$image_extensions_array = explode(' ', $image_extensions);
+	$checkstr = "";
+	foreach ($image_extensions_array as $count => $exe) {
+		$checkstr .= "(ext != '.$exe') && ";
+	}
+	$checkstr = rtrim($checkstr, ' && ');
+
+	// HTML GUI, js changes form encoding and adds error check
+	$form = "<script type='text/javascript' language='javascript'>
+		<!--
+		
+		function uploadPic() {
+			document.profile.enctype = 'multipart/form-data';
+			var upload = document.profile.picture.value;
+			upload = upload.toLowerCase();
+			var ext = upload.substring((upload.length-4),(upload.length));
+				if ($checkstr){
+					alert('Please upload an image with one of the following extentions: $image_extensions');
+					
+				}
+		}
+		//-->
+		</script>
+		<fieldset class='options'>
+		<legend>Fotograf&iacute;a</legend>
+		<p>Actual: <br />
+		<img src='$current_avatar' /><br /></p>
+		<p>Suba una nueva fotograf&iacute;a:  <input type='file' name='picture' onchange='uploadPic();' /><br />
+			Nota: <strong>Las im&aacute;genes deben estar en formato '.jpg .gif .png'. Si el tama&ntilde;o de la imagen excede el maximo, ésta ser&aacute; automaticamente rechazada.</strong><br />
+			Compatibilidad con navegadores: esta opci&aacute;n est&aacute; inhabilitada para <strong>Internet Explorer</strong>. Estamos trabajando en ello, mientras tanto recomendamos utilizar un navegador que respete los est&aacute;ndares como <a href='www.mozilla.com/firefox/'>Firefox</a> o <a href='http://opera.com'>Opera</a>.
+		</p>
+		</fieldset>";
+	return $form;
+}
+
+
+//*** INTERNAL FUNCTION: stores pic submitted via profile editing page
+function upload_feevy_pic() {
+	global $image_dir, $user_ID, $image_extensions, $profile_picture_options;
+	
+	$raw_name = (isset($_FILES['picture']['name'])) ? $_FILES['picture']['name'] : "";
+	
+	// if file was sumbitted, continue
+	if ($raw_name != "") {
+		// build the path and filename 		
+		$clean_name = ereg_replace("[^a-z0-9._]", "", ereg_replace(" ", "_", ereg_replace("%20", "_", strtolower($raw_name))));
+		$file_ext = substr(strrchr($clean_name, "."), 1);
+		$file_path = clean_path(ABSPATH . '/' . $image_dir . '/' . $user_ID . '.' . $file_ext);
+	// store file
+		if(filesize($_FILES['picture']['tmp_name']) > $profile_picture_options['maximum_size'] * 1000)
+		{
+			unlink($_FILES['picture']['tmp_name']);
+			die("<p><strong>Error:</strong> Profile pic size is above the maximum size " . $profile_picture_options['maximum_size'] . "KB.</code></p><p>Please <a href='javascript:history.go(-1)'>go back</a> and try again.");//Stolen from Andrew Ferguson 
+			return;
+		}
+		
+		$dimensions = getimagesize($_FILES['picture']['tmp_name']);
+		
+		if (extension_loaded('gd') && function_exists('gd_info')) {
+				
+			switch ($file_ext) {
+				case "jpg": $image = imagecreatefromjpeg($_FILES['picture']['tmp_name']);
+				break;
+		
+				case "png": $image = imagecreatefrompng($_FILES['picture']['tmp_name']);
+				break;
+		
+				case "gif": $image = imagecreatefromgif($_FILES['picture']['tmp_name']);
+				break;
+				
+				default:
+				unlink ($file_path);
+        		die("<p><strong>Error:</strong> Profile pic size is larger than Maximum Width/Height of " . $profile_picture_options['maximum_width'] . " pixels and cannot be resized.</code></p><p>Please <a href='javascript:history.go(-1)'>go back</a> and try again.");//Stolen from Andrew Ferguson 
+				break;
+			}
+			
+			if (isset($image))//Copied from PHP Manuals / with inspiration from myGallery
+			{
+				if($dimensions[0] > $profile_picture_options['maximum_width'] || $dimensions[1] > $profile_picture_options['maximum_width']){
+				if ($dimensions[0] >= $dimensions[1]) {
+        			$resize_x = $profile_picture_options['maximum_width'];
+          			$resize_y = (int) ($dimensions[1]*($profile_picture_options['maximum_width']/$dimensions[0]));
+        		} else {
+        			$resize_x = (int) ($dimensions[0]*($profile_picture_options['maximum_width']/$dimensions[1]));
+          			$resize_y = $profile_picture_options['maximum_width'];
+        		}
+        		if ($file_ext=='gif') {
+					$new_image = imagecreate($resize_x, $resize_y);
+				} else {
+					$new_image = imagecreatetruecolor($resize_x, $resize_y);
+				}  
+        		imagecopyresampled($new_image, $image, 0, 0, 0, 0, $resize_x, $resize_y, $dimensions[0], $dimensions[1]);
+				$image = $new_image;
+				}
+				
+				// Clean previous files
+				unlink(clean_path(ABSPATH . '/' . $image_dir . '/' . $user_ID . '.gif'));
+				unlink(clean_path(ABSPATH . '/' . $image_dir . '/' . $user_ID . '.jpg'));
+				unlink(clean_path(ABSPATH . '/' . $image_dir . '/' . $user_ID . '.png'));
+				
+				switch ($file_ext) {
+				case "jpg":imagejpeg($image, "$file_path",80);
+				break;
+				case "png": imagepng($image, "$file_path",8);
+				break;
+				case "gif": imagegif($image, "$file_path");
+				break;
+				}
+			}
+    	} else {	
+    		unlink($_FILES['picture']['tmp_name']);
+        	die("<p><strong>Error:</strong> Profile pic size is larger than Maximum Width/Height of " . $profile_picture_options['maximum_width'] . " pixels and cannot be resized.</code></p><p>Please <a href='javascript:history.go(-1)'>go back</a> and try again.");//Stolen from Andrew Ferguson 
+		}
+	} else {
+		return false;
+	}
+}
+
+/*
+* ================
+*  Plugin actions 
+* ================
+*/
+
+if(isset($_POST['feevy_portal_update'])) {
+	upload_feevy_pic();
+	feevy_update_avatar();
+	feevy_update_tag();
+}
+
+//add_action('admin_menu', 'feevy_options');
+
+add_action('wpmu_new_blog','feevy_add_blog');
+add_action('wpmu_new_blog','feevy_update_tag');
+add_action('wpmu_new_blog','feevy_update_avatar');
+
+add_action('profile_update','feevy_update_tag_from_user');
+add_action('profile_update','feevy_update_avatar');
+
+add_action('publish_post', 'feevy_ping');
+?>
---
+++ wp-content/mu-plugins/feevy_soup.php
@@ -0,0 +1,389 @@
+<?php
+
+//	error_reporting(error_reporting() & ~E_STRICT);
+// helps run on php5. e_strict doesnt do any good anyway ;)
+
+/* =========================================
+
+  FeevySoup v1.0 (Feevy API library)
+  April 27 2007
+
+  Developed by
+  Gorka Julio
+  teketen@gmail.com, teketen.com
+
+  Alexandre Girard
+  alx.girard@gmail.com, alexgirard.com
+
+  Inspired by
+  DuckSoup (c) 2005-2006, Kailash Nadh (http://kailashnadh.name)
+
+Feevy website
+  http://www.feevy.com
+
+Documentation
+  http://www.feevy.com/api/
+
+========================================= */
+
+
+
+class feevySoup {
+
+  var $host = "www.feevy.com";  // Feevy API server host (no http:// and no trailing slash)
+  var $port = 80;	        // Feevy API server port
+
+  var $api_key;
+  var $type;
+  var $params;
+
+  var $path   = "";
+  var $query  = "";
+
+  var $get_attributes = 0; // Some API call only need to get xml attributes
+
+
+  // ============= creates a tree (array) from the given xml data
+  function xml2array($contents) {
+
+    if(!$contents) return array();
+
+    if(!function_exists('xml_parser_create')) {
+      print "'xml_parser_create()' function not found!";
+      return array();
+    }
+    //Get the XML parser of PHP - PHP must have this module for the parser to work
+    $parser = xml_parser_create();
+    xml_parser_set_option( $parser, XML_OPTION_CASE_FOLDING, 0 );
+    xml_parser_set_option( $parser, XML_OPTION_SKIP_WHITE, 1 );
+    xml_parse_into_struct( $parser, $contents, $xml_values );
+    xml_parser_free( $parser );
+
+    if(!$xml_values) return;//Hmm...
+
+    //Initializations
+    $xml_array = array();
+    $parents = array();
+    $opened_tags = array();
+    $arr = array();
+
+    $current = &$xml_array;
+
+    //Go through the tags.
+    foreach($xml_values as $data) {
+      unset($attributes,$value);//Remove existing values, or there will be trouble
+      extract($data);//We could use the array by itself, but this cooler.
+
+      $result = '';
+      if($this->get_attributes) {//The second argument of the function decides this.
+        $result = array();
+        if(isset($value)) $result['value'] = $value;
+
+        //Set the attributes too.
+        if(isset($attributes)) {
+          foreach($attributes as $attr => $val) {
+            if($this->get_attributes == 1) $result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
+          }
+        }
+      } elseif(isset($value)) {
+        $result = $value;
+      }
+
+      //See tag status and do the needed.
+      if($type == "open") {//The starting of the tag '<tag>'
+      $parent[$level-1] = &$current;
+
+      if(!is_array($current) or (!in_array($tag, array_keys($current)))) { //Insert New tag
+        $current[$tag] = $result;
+        $current = &$current[$tag];
+
+      } else { //There was another element with the same tag name
+        if(isset($current[$tag][0])) {
+          array_push($current[$tag], $result);
+        } else {
+          $current[$tag] = array($current[$tag],$result);
+        }
+        $last = count($current[$tag]) - 1;
+        $current = &$current[$tag][$last];
+      }
+
+      } elseif($type == "complete") { //Tags that ends in 1 line '<tag />'
+      //See if the key is already taken.
+      if(!isset($current[$tag])) { //New Key
+        $current[$tag] = urldecode($result);
+
+      } else { //If taken, put all things inside a list(array)
+        if((is_array($current[$tag]) and $this->get_attributes == 0)//If it is already an array...
+        or (isset($current[$tag][0]) and is_array($current[$tag][0]) and $this->get_attributes == 1)) {
+          array_push($current[$tag],urldecode($result)); // ...push the new element into that array.
+        } else { //If it is not an array...
+          $current[$tag] = array($current[$tag],urldecode($result)); //...Make it an array using using the existing value and the new value
+        }
+      }
+
+      } elseif($type == 'close') { //End of tag '</tag>'
+      $current = &$parent[$level-1];
+    }
+  }
+  
+  return($xml_array);
+}
+
+// ============= Prepares the query based on the given api/parameters
+function prepare_query() {
+
+  // == No API type has been specified
+  if(!$this->type) {
+    return false;
+  }
+
+  // urlencode the parameters
+  if(isset($this->params) && is_array($this->params)) {
+    while(list($key, $value) = each($this->params)) {
+      $this->params[$key]=urlencode($value);
+    }
+  }
+
+  // do not get attributes by default
+  $get_attributes = 0;
+
+  // These routine prepares the query for each API call
+  switch($this->type) {
+
+    // ============== register_user api call
+    case 'register_user':
+    $view_key['email']=(!isset($this->params['email'])) ? '' : "&email=".$this->params['email'];
+    $view_key['password']=(!isset($this->params['password'])) ? '' : "&password=".$this->params['password'];
+    // == Prepare the url for view_key api query
+    $path   = "/api/register_user";
+    $query  = "api_key={$this->api_key}{$view_key['email']}{$view_key['password']}";
+    $get_attributes = 1;
+    break;
+
+    // ============== verify_key api call
+    case 'verify_key':
+    // == Prepare the url for verify_key api query
+    $path   = "/api/verify_key";
+    $query  = "api_key={$this->api_key}";
+    $get_attributes = 1;
+    break;
+
+    // ============== view_key api call
+    case 'view_key':
+    $view_key['email']=(!isset($this->params['email'])) ? '' : "&email=".$this->params['email'];
+    $view_key['password']=(!isset($this->params['password'])) ? '' : "&password=".$this->params['password'];
+    // == Prepare the url for view_key api query
+    $path   = "/api/view_key";
+    $query  = "api_key={$this->api_key}{$view_key['email']}{$view_key['password']}";
+    $get_attributes = 1;
+    break;
+
+    // ============== list_feed api call
+    case 'list_feed':
+    // == Prepare the url for list_feed api query
+    $path   = "/api/list_feed";
+    $query  = "api_key={$this->api_key}";
+    break;
+
+    // ============== get_feed api call
+    case 'get_feed':
+    $get_feed['feed_url']=(!isset($this->params['feed_url'])) ? '' : "&feed_url=".$this->params['feed_url'];
+    // == Prepare the url for get_feed api query
+    $path   = "/api/get_feed";
+    $query  = "api_key={$this->api_key}{$get_feed['feed_url']}";
+    break;
+
+    // ============== add_feed api call
+    case 'add_feed':
+    $add_feed['url']=(!isset($this->params['url'])) ? '' : "&url=".$this->params['url'];
+    $add_feed['href']=(!isset($this->params['href'])) ? '' : "&href=".$this->params['href'];
+    // == Prepare the url for add_feed api query
+    $path   = "/api/add_feed";
+    $query  = "api_key={$this->api_key}{$add_feed['url']}{$add_feed['href']}";
+    break;
+
+    // ============== delete_feeds api call
+    case 'delete_feeds':
+    $delete_feeds['feeds_id']=(!isset($this->params['feeds_id'])) ? '' : "&feeds_id=".$this->params['feeds_id'];
+    // == Prepare the url for delete_feeds api query
+    $path   = "/api/delete_feeds";
+    $query  = "api_key={$this->api_key}{$delete_feeds['feeds_id']}";
+    break;
+
+    // ============== delete_feed api call
+    case 'delete_feed':
+    $delete_feeds['feeds_id']=(!isset($this->params['feed_id'])) ? '' : "&feeds_id=".$this->params['feed_id'];
+    // == Prepare the url for delete_feeds api query
+    $path   = "/api/delete_feeds";
+    $query  = "api_key={$this->api_key}{$delete_feeds['feeds_id']}";
+    break;
+
+    // ============== edit_tags api call
+    case 'edit_tags':
+    $edit_tags['feed_url']=(!isset($this->params['feed_url'])) ? '' : "&feed_url=".$this->params['feed_url'];
+    $edit_tags['tag_list']=(!isset($this->params['tag_list'])) ? '' : "&tag_list=".$this->params['tag_list'];
+    // == Prepare the url for edit_tags api query
+    $path   = "/api/edit_tags";
+    $query  = "api_key={$this->api_key}{$edit_tags['feed_url']}{$edit_tags['tag_list']}";
+    break;
+
+    // ============== edit_avatar api call
+    case 'edit_avatar':
+    $edit_avatar['feed_url']=(!isset($this->params['feed_url'])) ? '' : "&feed_url=".$this->params['feed_url'];
+    $edit_avatar['avatar_url']=(!isset($this->params['avatar_url'])) ? '' : "&avatar_url=".$this->params['avatar_url'];
+    // == Prepare the url for edit_avatar api query
+    $path   = "/api/edit_avatar";
+    $query  = "api_key={$this->api_key}{$edit_avatar['feed_url']}{$edit_avatar['avatar_url']}";
+    break;
+
+    // ============== No proper type ?
+    default:
+    return false;
+    break;
+
+  }
+
+  $this->query=$query;
+  $this->path=$path;
+  $this->get_attributes=$get_attributes;
+}
+
+// ============= Fetches the data over http from Feevy
+function fetch_content() {
+
+  // =========== Fetch the data
+  $data="";
+
+  $http_request  = "POST {$this->path} HTTP/1.0\r\n";
+  $http_request .= "Host: {$this->host}\r\n";
+  $http_request .= "Content-Type: application/x-www-form-urlencoded; charset=UTF-8\r\n";
+  $http_request .= "Content-Length: " . strlen($this->query) . "\r\n";
+  $http_request .= "User-Agent: Feevy Soup\r\n";
+  $http_request .= "\r\n";
+  $http_request .= $this->query;
+
+  if( false != ( $fs = @fsockopen($this->host, $this->port, $errno, $errstr, 10) ) ) {
+    fwrite($fs, $http_request);
+
+    while ( !feof($fs) )
+      $data .= fgets($fs, 1160); // One TCP-IP packet
+    fclose($fs);
+    $data = explode("\r\n\r\n", $data, 2);
+  }
+
+  return $data[1];
+}
+
+// ============= The core function that processes Feevy data
+function get_content() {
+  
+  $result="";
+  
+  // == No API type has been specified
+  if(!$this->type) {
+    return false;
+  }
+
+  $this->prepare_query();	// Prepare the query with all necessary variables
+  $data=$this->fetch_content();	// get data from Feevy
+
+  if(!$data) return false;
+  
+  $xml_array=$this->xml2array($data);
+  if(!$xml_array) return false;
+
+  // parses and formats data for each API
+  switch($this->type) {
+
+    // ============== register_user api call
+    case 'register_user':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while registering this user";
+    }
+    break;
+
+    // ============== verify_key api call
+    case 'verify_key':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while verifying this api key: {$this->api_key}";
+    }
+    break;
+
+    // ============== view_key api call
+    case 'view_key':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while viewing this user api key";
+    }
+    break;
+
+    // ============== list_feed api call
+    case 'list_feed':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while retrieving list of feeds with {$this->api_key}";
+    }
+    break;
+
+    // ============== get_feed api call
+    case 'get_feed':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while retrieving feed with {$this->api_key}";
+    }
+    break;
+
+    // ============== add_feed api call
+    case 'add_feed':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while adding feed";
+    }
+    break;
+
+    // ============== delete_feeds api call
+    case 'delete_feeds':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while deleting feeds";
+    }
+    break;
+
+    // ============== edit_tags api call
+    case 'edit_tags':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while editing tags";
+    }
+    break;
+
+    // ============== edit_avatar api call
+    case 'edit_avatar':
+    if(isset($xml_array['feevy']) && is_array($xml_array['feevy'])) {
+      $result['result']=$xml_array['feevy'];
+    } else {
+      $result['error'] = "An error occured while editing avatar";
+    }
+    break;
+    
+    default:
+      $result['error'] = "An error occured while fetching API type";
+    break;
+  }
+
+  return $result;
+}
+
+}
+
+?>
